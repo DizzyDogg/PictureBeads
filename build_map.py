202f9eef26f58be25700eb620b7ba03d7f3c9e82
Daniel Fackrell <unlearned@gmail.com>
2020-04-02 11:51:24 -0600

    Initial commit

---
+++ build_map.py
@@ -0,0 +1,22 @@
+#!/bin/env python3
+
+from PIL import Image
+import numpy
+
+
+layers = Image.open("Replacement_map.gif")
+in_image = Image.open("cropped_and_scaled.png")
+
+layers_array = numpy.asarray(layers)
+in_array = numpy.asarray(in_image)
+
+print(in_array)
+
+print(len(in_array))
+print(in_array[0])
+print(len(in_array[0]))
+
+out_array = in_array
+out_image = Image.fromarray(out_array, "RGB")
+
+
