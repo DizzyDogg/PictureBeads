c3674242c1bbf9016e07389a98c4354eba056a55
Daniel Fackrell <unlearned@gmail.com>
2020-04-07 12:31:26 -0600

    Added color channel modification, dithering

--- build_map.py
+++ build_map.py
@@ -1,22 +1,45 @@
 #!/bin/env python3
 
 from PIL import Image
-import numpy
+
+from colors import Colors
 
 
-layers = Image.open("Replacement_map.gif")
+OUTPUT_WIDTH = 40
+OUTPUT_HEIGHT = 50
+COLORS_FILENAME = "bead_colors_perler_standard.json"
+
+# Get image
 in_image = Image.open("cropped.png")
 
-layers_array = numpy.asarray(layers)
-in_array = numpy.asarray(in_image)
+# Resize image
+scaled_image = in_image.resize((40, 50), Image.LANCZOS)
 
-print(in_array)
+# Generate a palette from available colors
+colors = Colors(COLORS_FILENAME)
+palette_image = Image.new('P', (16, 16))
+palette_image.putpalette(colors.get_palette())
 
-print(len(in_array))
-print(in_array[0])
-print(len(in_array[0]))
+# Adjust channels
+R = 1.1
+G = 1.1
+B = 1.1
+adjusted_image = scaled_image.convert(
+    mode="RGB",
+    matrix=(R, 0, 0, 0,
+            0, G, 0, 0,
+            0, 0, B, 0)
+)
 
-out_array = in_array
-out_image = Image.fromarray(out_array, "RGB")
+# Convert to bead colors
+out_image = adjusted_image.convert(
+    mode="P",
+    dither=Image.FLOYDSTEINBERG,
+    palette=palette_image
+)
 
+# Display for debugging purposes
+out_image.show()
+
+# Write out to disk
 out_image.save('test.png', 'PNG')
