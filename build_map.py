aa69339f7af11786cf3c4ac4d77537fa9079199f
Daniel Fackrell <unlearned@gmail.com>
2020-04-23 14:23:50 -0600

    Refactored into reusable functions

--- build_map.py
+++ build_map.py
@@ -3,67 +3,79 @@
 from collections import defaultdict
 
 from PIL import Image
-from PIL.ImageOps import mirror
 
 from colors import Colors
 
-
+TILE_HEIGHT = TILE_WIDTH = 60
 OUTPUT_SIZE = (40, 50)
 COLORS_FILENAME = "bead_colors_perler_standard.json"
 
-# Get image
-in_image = Image.open("cropped.png")
-
-# Resize image
-scaled_image = in_image.resize((40, 50), Image.LANCZOS)
-
 # Generate a palette from available colors
-colors = Colors(COLORS_FILENAME)
-palette_image = colors.get_palette()
+COLORS = Colors(COLORS_FILENAME)
+PALETTE_IMAGE = COLORS.get_palette()
 
-# Adjust channels
-R = 1.1
-G = 1.1
-B = 1.1
-adjusted_image = scaled_image.convert(
-    mode="RGB",
-    matrix=(R, 0, 0, 0,
-            0, G, 0, 0,
-            0, 0, B, 0)
-)
 
-# Convert to bead colors
-dithered_image = adjusted_image.quantize(palette=palette_image, method=0)
-
-# Flip image left to right
-flipped_image = mirror(dithered_image)
-
-# Generate template
-out_image = Image.new(
-    "RGB",
-    # Output size
-    tuple(l * r for l, r in zip(OUTPUT_SIZE, colors.image_size)),
-)
-width, height = flipped_image.size
-for y in range(height):
-    for x in range(width):
-        pixel = flipped_image.getpixel((x, y))
-        color_name = colors.get_color_name(pixel)
-        out_image.paste(
-            colors.color_lookup[color_name]["image"],
-            (x * 60, y * 60)
+def adjust_channels(image, red=1.1, green=1.1, blue=1.1):
+    return image.convert(
+        mode="RGB",
+        matrix=(red,   0,     0,     0,
+                0,     green, 0,     0,
+                0,     0,     blue,  0)
         )
 
-# Display for debugging purposes
-# out_image.show()
 
-# Count beads of each color
-color_counts = defaultdict(int)
-for y in range(height):
-    for x in range(width):
-        pixel = dithered_image.getpixel((x, y))
-        color_name = colors.get_color_name(pixel)
-        color_counts[color_name] += 1
+def dither(image):
+    return image.quantize(palette=PALETTE_IMAGE, method=0)
 
-# Write out to disk
-out_image.save('test.png', 'PNG')
+
+def resize(image, output_size=OUTPUT_SIZE):
+    return image.resize(output_size, Image.LANCZOS)
+
+
+def flip(image):
+    from PIL.ImageOps import mirror
+
+    return mirror(image)
+
+
+def generate_template(image):
+    # Generate template
+    out_image = Image.new(
+        "RGB",
+        # Output size
+        tuple(l * r for l, r in zip(OUTPUT_SIZE, COLORS.image_size)),
+    )
+    width, height = image.size
+    for y in range(height):
+        for x in range(width):
+            pixel = image.getpixel((x, y))
+            color_name = COLORS.get_color_name(pixel)
+            out_image.paste(
+                COLORS.color_lookup[color_name]["image"],
+                (x * TILE_WIDTH, y * TILE_HEIGHT)
+            )
+    return out_image
+
+
+def count_beads(image):
+    width, height = image.size
+    color_counts = defaultdict(int)
+    for y in range(height):
+        for x in range(width):
+            pixel = image.getpixel((x, y))
+            color_name = COLORS.get_color_name(pixel)
+            color_counts[color_name] += 1
+    return color_counts
+
+
+def generate_pixelart(image):
+    return dither(adjust_channels(resize(image)))
+
+
+if __name__ == "__main__":
+    in_image = Image.open("cropped.png")
+
+    pixelart = generate_pixelart(in_image)
+
+    print(count_beads(pixelart))
+    generate_template(pixelart).save("test.png", "PNG")
