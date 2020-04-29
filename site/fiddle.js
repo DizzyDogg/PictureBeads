97397fbd4c627d38e244d9724e0c15462e655780
dizzydogg <jeffthetenor@yahoo.com>
2020-04-28 21:53:47 -0600

    IT WORKS!!

--- site/fiddle.js
+++ site/fiddle.js
@@ -1,6 +1,7 @@
 ///////////////// CROPPIE SECTION ///////////////////
 var width = 40;
 var height = 50;
+var apiURL = 'http://localhost:8000/generate_pixelart';
 var source = $('#cropper-tool').croppie({
   viewport: { width: 160, height: 200 },
   boundary: { width: 400, height: 400 },
@@ -20,28 +21,31 @@ function readFile(input) {
   }
 }
 $('.actionUpload input').on('change', function() { readFile(this); });
-$('.actionDone').on('click', function() {
-  // $('.actionDone').toggle();
-  // $('.actionUpload').toggle();
-  source.croppie('result', {
-      type: 'base64',
-      size: { 'width': width, 'height': height },
-      format: 'png'
-  }).then(function(foo){
-      var data = foo.split(',');
-      var uploadedImage = data[1];
-      var mimeType = data[0].split(';')[0].split(':')[1];
+$('.actionDone').on('click', generatePixelImage);
+       
+// create the new image with the RGB color channels
+function generatePixelImage() {
+    // $('.actionDone').toggle();
+    // $('.actionUpload').toggle();
+    source.croppie('result', {
+        type: 'base64',
+        size: { 'width': width, 'height': height },
+        format: 'png'
+    }).then(function(foo){
+        var data = foo.split(',');
+        var uploadedImage = data[1];
+        var mimeType = data[0].split(';')[0].split(':')[1];
 
-      var postData = JSON.stringify({ 'image': uploadedImage });
-      jQuery.post(
-          'http://localhost:8000/generate_pixelart?',
-          postData,
-          function(returnData, textStatus, jQueryXHR){
-              console.log(returnData);
-          }
-      )
-  });
-})
+        var postData = JSON.stringify({ 'image': uploadedImage });
+        var queryStr =  `?red=${red.value/100}&green=${green.value/100}&blue=${blue.value/100}`;
+        jQuery.post( apiURL + queryStr, postData, displayReturnImage);
+    });
+}
+
+function displayReturnImage(returnData, textStatus, jQueryXHR){
+    var srcStr = "data:image/png;base64," + returnData.image
+    $('#result-img').attr('src', srcStr);
+}
 
 ///////////////// POWERANGE SECTION ///////////////////
 // find element
@@ -58,16 +62,14 @@ var greenBox = document.querySelector("#green-box");
 var blueBox = document.querySelector("#blue-box");
 
 var redArgs = {
-  min: 0,
-  max: 255,
-  start: 40,
-  step: 10
+  min: 80,
+  max: 120,
+  start: 100,
+  step: 1
 };
 
 var greenArgs = Object.assign({}, redArgs);
-greenArgs.start = 100;
-var blueArgs = Object.assign({}, redArgs);
-blueArgs.start = 200;
+var blueArgs =  Object.assign({}, redArgs);
 
 // set default values
 var red1 = new Powerange(red, redArgs);
@@ -84,6 +86,6 @@ function setBGColor() {
 }
 
 var red0 = $("#red-bar");
-$(".slider-container").on("mouseup", function() { setBGColor() })
+$(".slider-container").on("mouseup", generatePixelImage);
 
 setBGColor();
